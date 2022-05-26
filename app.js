24fffc4d731d95415c95f77e311417a57546239d
Samir Thapa <thapasamirm577@gmail.com>
2022-05-26 20:14:50 +0545

    ready to deploy

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use(express.static("public"));
 // })
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("clinet/build"));
-    app.get("*", (req,res)=>{
-        res.sendFile(path.resolve('/client','/build','/index.html'));
-    })
+    app.use(express.static("client/build"));
+    // app.get("*", (req,res)=>{
+    //     res.sendFile(path.resolve('/client','/build','/index.html'));
+    // })
 }
 
 app.use("/api/user", userRouter);
