1249057596e8e93a49e7147cbeb8efb1e8da5855
Samir Thapa <thapasamirm577@gmail.com>
2022-05-26 20:30:33 +0545

    ready to deploy

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   // development
-  // baseURL: process.env.NODE_ENV === "production"?"https://linkedinclone577.herokuapp.com":"http://localhost:5000",
+  baseURL: process.env.NODE_ENV === "production"?"https://linkedinclone577.herokuapp.com":"http://localhost:5000",
   withCredentials: true,
 });
