7d14c2bcaf194683926259ac89986189e64fca7e
Samir Thapa <thapasamirm577@gmail.com>
2022-05-26 20:34:06 +0545

    ready to deploy

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(express.static("public"));
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
-    // app.get("*", (req,res)=>{
-    //     res.sendFile(path.resolve('/client','/build','/index.html'));
-    // })
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve('client','build','index.html'));
+    })
 }
 
 app.use("/api/user", userRouter);
